fa12336253c8be910127aef7846433643d41ad98
rizkiito <125981746+rizkiito@users.noreply.github.com>
2023-02-27 09:49:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Personal Webpage</title>
+  
+  
+  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css'>
+
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+  <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css"></link>
+<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet" type="text/css"></link>
+<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet" type="text/css"></link>
+
+<div class = "container-fluid">
+  <div id = "home-page" class = "home-page">
+    <div class = "box row">
+      <div class = "col-xs-6">
+        <h1>Rizki Minta Maito<h2><i>"Halo saya Rizki Semeter 6 jurusan Teknik Informatika. Universitas Malikussaleh" </i></h2></h1>
+      </div>
+      <div class = "col-xs-6">
+       <img class = "smaller-image" src="assets/pp.jpeg" alt=""> </img>
+    </div>
+    </div>
+    </div>
+<div id = "activities-page" class = "activities-page">
+      <div class="container-fluid">
+        <h3 class="text-center">Activities page</h3>
+         <ul class = "grid">
+          <li>
+            <img src="assets/pp2.jpg"alt='https://kampusmerdeka.kemdikbud.go.id/'></img> </li>
+          <li>
+           <img src="assets/pp3.png"alt='https://gits.id/'></img> </li>
+          
+      </div>
+  </div>
+<div id = "contact-page" class = "contact-page">
+    <div class = " box-3 container-fluid">
+      <h3 class="text-center">Contact Page</h3>
+        <input type="text" placeholder="Name" required>
+       <input type="text" placeholder="Email Adress" required>
+       <input type="text" placeholder="Phone Number" required>
+       <input type="text" placeholder="Message" required>
+  <button type="submit">Submit</button>
+</form>
+  <p></p>
+  <p> <i>Want to Contact Me? Fill Out the Form and I will get back to you ASAP! </i></p>
+  </div>
+</div>
+  <div id = "social-page" class="social-page">
+    <div class ="box-4 ">
+      <h4> Social Media: </h4>
+      <div class="row">
+    <div class="col-xs-4">
+      <form action= "https://instagram.com/rizkiito?igshid=ZDdkNTZiNTM=">
+      <button class="btn btn-block btn-info"><i class="fa fa-instagram" aria-hidden="true"></i>Ig</button>
+      </form>
+    </div>
+    <div class="col-xs-4">
+      <form action= "https://github.com/rizkiito">
+      <button class="btn btn-block btn-info"><i class="fa fa-github" aria-hidden="true"></i> GitHub</button>
+      </form>
+    </div>
+    <div class="col-xs-4">
+      <form action= "https://twitter.com/Rizkiito22?t=1_Qmp0aDsNoMWn5M6MbS2w&s=08">
+        <button class="btn btn-block btn-info"><i class="fa fa-twitter" aria-hidden="true"></i> twitter</button>
+      </form>
+    </div>
+  </div>
+    </div>
+</div>
+  <div class = "navbar row">
+    <div class = "col-xs-4">
+  <a href="#contact-page" class = "button">Contact</a>
+  <a href="#activities-page" class = "button">Activity</a>
+  <a href="#home-page" class = "button">Home</a>
+    </div>
+    </div>
+</div>
+  
+  
+</body>
+</html>
